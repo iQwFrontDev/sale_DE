5bd330026c5dd282d047a4974d946920a60c6d73
iqw <iqwgame1@gmail.com>
2024-09-30 22:13:40 +0400

    Initial commit

--- sale.py
+++ sale.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import seaborn as  sns
+import openpyxl
+import matplotlib.pyplot as plt
+
+
 def read_sales_data(file_path):
     return pd.read_excel(file_path).to_dict()
 
@@ -22,3 +27,12 @@ def sales_over_time(sales_data):
 x = read_sales_data('/home/user/sale_DE/sale_DE/Book1.xlsx')
 print(total_sales_per_product(x))
 print(sales_over_time(x))
+
+key_all_sales = total_sales_per_product(x).keys();
+val_all_sales = total_sales_per_product(x).values();
+sns.barplot(x=key_all_sales, y=val_all_sales);
+
+key_date_sales = sales_over_time(x).keys();
+val_sdate_ales = sales_over_time(x).values();
+sns.barplot(x=key_date_sales, y=val_sdate_ales);
+plt.xticks(rotation=45);
